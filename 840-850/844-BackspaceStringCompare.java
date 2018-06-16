eb233c212a3729fecb0ed2bc5a79c1ff64eaea15
Xiaohan Liu <xliu4@snapchat.com>
2018-06-15 18:14:34 -0700

    backspace string compare

---
+++ 840-850/844-BackspaceStringCompare.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return translate(S).equals(translate(T));
+    }
+    
+    private String translate(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '#') {
+                sb.append(s.charAt(i));
+            } else {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
