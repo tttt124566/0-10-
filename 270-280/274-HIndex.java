4ccd3d7fd36cfd488befe8f6d5ee7d1410353c86
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-06-24 19:37:37 -0700

    Create 274-HIndex.java

---
+++ 270-280/274-HIndex.java
@@ -0,0 +1,52 @@
+/*
+
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+Example:
+
+Input: citations = [3,0,6,1,5]
+Output: 3 
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had 
+             received 3, 0, 6, 1, 5 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each and the remaining 
+             two with no more than 3 citations each, her h-index is 3.
+*/
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        // int n = citations.length;
+        // int[] papers = new int[n + 1];
+        // // counting papers for each citation number
+        // for (int c: citations)
+        //     papers[Math.min(n, c)]++;
+        // // finding the h-index
+        // int k = n;
+        // for (int s = papers[n]; k > s; s += papers[k])
+        //     k--;
+        // return k;
+        /*
+        Papers that have more than n citations will store in array[n].
+Go through the array based on h index definition: array[i]>=i, find the max value of i.
+        */
+        
+        int n = citations.length;
+        int[] buckets = new int[n+1];
+        for(int c : citations) {
+            if(c >= n) {
+                buckets[n]++;
+            } else {
+                buckets[c]++;
+            }
+        }
+        int count = 0;
+        for(int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if(count >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
