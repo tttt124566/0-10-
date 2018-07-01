1fe3e158a52f816be385529e75d97bdd2c7b3f7d
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-06-30 20:01:58 -0700

    Create 490-TheMaze.java

---
+++ 490-500/490-TheMaze.java
@@ -0,0 +1,86 @@
+/*
+There is a ball in a maze with empty spaces and walls. The ball can go through empty spaces by rolling up, down, left or right, but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction.
+
+Given the ball's start position, the destination and the maze, determine whether the ball could stop at the destination.
+
+The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may assume that the borders of the maze are all walls. The start and destination coordinates are represented by row and column indexes.
+
+Example 1
+
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (4, 4)
+
+Output: true
+Explanation: One possible way is : left -> down -> left -> down -> right -> down -> right.
+
+Example 2
+
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (3, 2)
+
+Output: false
+Explanation: There is no way for the ball to stop at the destination.
+
+Note:
+There is only one ball and one destination in the maze.
+Both the ball and the destination exist on an empty space, and they will not be at the same position initially.
+The given maze does not contain border (like the red rectangle in the example pictures), but you could assume the border of the maze are all walls.
+The maze contains at least 2 empty spaces, and both the width and height of the maze won't exceed 100.
+*/
+
+// bfs 
+class Solution {
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        if (maze == null || maze.length == 0) {
+            return false;
+        }
+        int m = maze.length;
+        int n = maze[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Queue<Point> queue = new LinkedList<>();
+        if (start[0] == destination[0] && start[1] == destination[1]) {
+            return true;
+        }
+        visited[start[0]][start[1]] = true;
+        queue.offer(new Point(start[0], start[1]));
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            for (int[] dir : dirs) {
+                int xx = point.x;
+                int yy = point.y;
+                while (xx >= 0 && yy >= 0 && xx < m && yy < n && maze[xx][yy] != 1) {
+                    xx = xx + dir[0];
+                    yy = yy + dir[1];
+                }
+                xx = xx - dir[0];
+                yy = yy - dir[1];
+                if (visited[xx][yy]) {
+                    continue;
+                }
+                if (xx == destination[0] && yy == destination[1]) {
+                    return true;
+                }
+                visited[xx][yy] = true;
+                queue.offer(new Point(xx, yy));
+            }
+        }
+        return false;
+    }
+} 
