268a8359ae4592f536d7a7645466144e777f5e55
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-02 20:13:31 -0700

    Create 668-KthSmallestNumberInMultiplicationTable.java

---
+++ 660-670/668-KthSmallestNumberInMultiplicationTable.java
@@ -0,0 +1,54 @@
+/*
+Nearly every one have used the Multiplication Table. But could you find out the k-th smallest number quickly from the multiplication table?
+
+Given the height m and the length n of a m * n Multiplication Table, and a positive integer k, you need to return the k-th smallest number in this table.
+
+Example 1:
+Input: m = 3, n = 3, k = 5
+Output: 
+Explanation: 
+The Multiplication Table:
+1	2	3
+2	4	6
+3	6	9
+
+The 5-th smallest number is 3 (1, 2, 2, 3, 3).
+Example 2:
+Input: m = 2, n = 3, k = 6
+Output: 
+Explanation: 
+The Multiplication Table:
+1	2	3
+2	4	6
+
+The 6-th smallest number is 6 (1, 2, 2, 3, 4, 6).
+Note:
+The m and n will be in the range [1, 30000].
+The k will be in the range [1, m * n]
+*/
+
+
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int low = 1;
+        int high = m * n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = getCount(mid, m, n);
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+    
+    private int getCount(int mid, int m, int n) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(mid / i, n);
+        }
+        return count;
+    }
+}
