ecb010d5d7ed884edc448c87ed996832d8470074
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-03 21:48:52 -0700

    Create 864-RandomPickWithBlacklist.java

---
+++ 860-870/864-RandomPickWithBlacklist.java
@@ -0,0 +1,79 @@
+/*
+Given a blacklist B containing unique integers from [0, N), write a function to return a uniform random integer from [0, N) which is NOT in B.
+
+Optimize it such that it minimizes the call to system’s Math.random().
+
+Note:
+
+1 <= N <= 1000000000
+0 <= B.length < min(100000, N)
+[0, N) does NOT include N. See interval notation.
+Example 1:
+
+Input: 
+["Solution","pick","pick","pick"]
+[[1,[]],[],[],[]]
+Output: [null,0,0,0]
+Example 2:
+
+Input: 
+["Solution","pick","pick","pick"]
+[[2,[]],[],[],[]]
+Output: [null,1,1,1]
+Example 3:
+
+Input: 
+["Solution","pick","pick","pick"]
+[[3,[1]],[],[],[]]
+Output: [null,0,0,2]
+Example 4:
+
+Input: 
+["Solution","pick","pick","pick"]
+[[4,[2]],[],[],[]]
+Output: [null,1,3,1]
+Explanation of Input Syntax:
+
+The input is two lists: the subroutines called and their arguments. Solution's constructor has two arguments, N and the blacklist B. pick has no arguments. Arguments are always wrapped with a list, even if there aren't any.
+
+*/
+
+class Solution {
+    private Map<Integer, Integer> map;
+    
+    private Random r;
+    int M;
+    public Solution(int N, int[] blacklist) {
+        map = new HashMap<>();
+        r = new Random();
+        
+        for (int i : blacklist) {
+            map.put(i, -1);
+        }
+        M = N - blacklist.length;
+        
+        for (int i : blacklist) {
+            if (i < M) {
+                while (map.containsKey(N - 1)) {
+                    N--;
+                }
+                map.put(i, N - 1);
+                N--;
+            }
+        }
+
+    }
+    
+    public int pick() {
+        int p = r.nextInt(M);
+        if (map.containsKey(p))
+            return map.get(p);
+        return p;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(N, blacklist);
+ * int param_1 = obj.pick();
+ */
