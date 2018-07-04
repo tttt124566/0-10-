9d7be1dc1c4056eaf26cb22c26217a10c58adf0b
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-03 22:13:53 -0700

    Update 261-GraphValidTree.java

--- 260-270/261-GraphValidTree.java
+++ 260-270/261-GraphValidTree.java
@@ -10,8 +10,6 @@ Example 2:
 Input: n = 5, and edges = [[0,1], [1,2], [2,3], [1,3], [1,4]]
 Output: false
 Note: you can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0,1] is the same as [1,0] and thus will not appear together in edges.
-
-
 */
 
 class Solution {
