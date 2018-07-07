38465397bd011da8730f4eebd165228a15481264
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-07 15:54:44 -0700

    Create 251-Flatten2DVector.java

---
+++ 250-260/251-Flatten2DVector.java
@@ -0,0 +1,44 @@
+/*
+Implement an iterator to flatten a 2d vector.
+
+Example:
+
+Input: 2d vector =
+[
+  [1,2],
+  [3],
+  [4,5,6]
+]
+Output: [1,2,3,4,5,6]
+Explanation: By calling next repeatedly until hasNext returns false, 
+             the order of elements returned by next should be: [1,2,3,4,5,6].
+*/
+
+public class Vector2D {
+
+    private Iterator<List<Integer>> i;
+    private Iterator<Integer> j;
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        i = vec2d.iterator();
+    }
+
+    public int next() {
+        return j.next();
+    }
+
+    public boolean hasNext() {
+        while ((j == null || !j.hasNext()) && i.hasNext())
+            j = i.next().iterator();
+        return j != null && j.hasNext();
+    }
+    
+    public void remove() {
+        while (j == null && i.hasNext()) {
+            j = i.next().iterator();
+        }
+        if (j != null) {
+            j.remove();
+        }
+    }
+}
