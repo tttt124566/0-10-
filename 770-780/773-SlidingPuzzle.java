4127e3bab336aeff1fb371b80785d5a41534c792
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-08 16:28:48 -0700

    Update 773-SlidingPuzzle.java

--- 770-780/773-SlidingPuzzle.java
+++ 770-780/773-SlidingPuzzle.java
@@ -151,3 +151,93 @@ class Solution {
         }
     }
 }
+
+/*
+import java.util.*;
+
+class SlidingGame {.本文原创自1point3acres论坛
+    private int[] dx = {-1, 0, 1, 0};
+    private int[] dy = {0, -1, 0, 1};. more info on 1point3acres
+
+    class Status {
+        int[][] matrix;
+        int x, y;
+        Status(int[][] m, int i, int j) {
+            matrix = new int[m.length][m[0].length];. 牛人云集,一亩三分地
+            for (int ii = 0; ii < m.length; ii++)
+                matrix[ii] = m[ii].clone();
+            x = i;
+            y = j;-google 1point3acres
+        }
+        String encodeMatrix() {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < matrix.length; i++)
+                for (int j = 0; j < matrix.length; j++)
+                    builder.append(matrix[j]).append(",");
+            return builder.toString();
+        }
+        void move(int i, int j) {
+            matrix[x][y] = matrix[j];.1point3acres网
+            matrix[j] = 0;
+            x = i;
+            y = j;. 牛人云集,一亩三分地
+        }
+    }
+
+    public boolean canSolve(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+来源一亩.三分地论坛. 
+        int startX = 0, startY = 0;
+        int[][] finalMatrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                finalMatrix[j] = i * m + j;
+                if (matrix[j] == 0) {
+                    startX = i;
+                    startY = j;
+                }
+            }.留学论坛-一亩-三分地
+        }
+
+        Status initial = new Status(matrix, startX, startY);. from: 1point3acres 
+        Status finals = new Status(finalMatrix, 0, 0);
+        Queue<Status> queue = new LinkedList<>();. visit 1point3acres for more.
+        queue.add(initial);. 1point3acres
+        Set<String> visited = new HashSet<>();
+        visited.add(initial.encodeMatrix());
+        System.out.println(initial.encodeMatrix());
+
+        while (!queue.isEmpty()) {
+            Status current = queue.poll();
+            System.out.println("F: " + current.encodeMatrix());
+            int xx = current.x, yy = current.y;
+            for (int i = 0; i < 4; i++) {
+                int x = xx + dx; 来源一亩.三分地论坛. 
+                int y = yy + dy;
+                if (x < 0 || x >= m || y < 0 || y >= n)
+                    continue;
+                current.move(x, y);
+                if (current.encodeMatrix().equals(finals.encodeMatrix()))
+                    return true;
+                if (!visited.contains(current.encodeMatrix())) {
+                    visited.add(current.encodeMatrix());
+                    System.out.println(current.encodeMatrix());
+                    queue.add(new Status(current.matrix, current.x, current.y));
+                }
+                current.move(xx, yy);
+            }
+        }
+
+        return false;
+    }
+
+
+    public final static void main(String[] args) {
+        SlidingGame sg = new SlidingGame();
+        int[][] matrix = new int[][]{{2, 3, 8}, {1, 4, 7}, {6, 0, 5}};. Waral 博客有更多文章,
+        System.out.println(sg.canSolve(matrix));
+    }
+
+}
+*/
