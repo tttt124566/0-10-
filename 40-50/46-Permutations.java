5829727aa2f79f8ecefa0d0bceeed704aa9b0b32
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-11 20:06:37 -0700

    Update 46-Permutations.java

--- 40-50/46-Permutations.java
+++ 40-50/46-Permutations.java
@@ -36,3 +36,33 @@ class Solution {
         }
     }
 }
+
+public class Solution {
+    public List<List<Integer>> permute(int[] num) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        permute(result, num, 0);
+        return result;
+    }
+    
+    private void permute(List<List<Integer>> result, int[] array, int start) {
+		if (start >= array.length) {
+			List<Integer> current = new ArrayList<Integer>();
+			for (int a : array) {
+			    current.add(a);
+			}
+			result.add(current);
+		} else {
+			for (int i = start; i < array.length; i++) {
+				swap(array, start, i);
+				permute(result, array, start+1);
+				swap(array, start, i);
+			}
+		}
+	}
+	
+	private void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
