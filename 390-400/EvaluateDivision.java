62d16bd7f3a9a5b441cca9f54277e2817cc7b831
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-15 20:43:28 -0700

    Create EvaluateDivision.java

---
+++ 390-400/EvaluateDivision.java
@@ -0,0 +1,59 @@
+/*
+Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
+
+Example:
+Given a / b = 2.0, b / c = 3.0. 
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? . 
+return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+
+The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries , where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
+
+According to the example above:
+
+equations = [ ["a", "b"], ["b", "c"] ],
+values = [2.0, 3.0],
+queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]. 
+The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
+
+
+*/
+
+class Solution {
+    public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        HashMap<String, ArrayList<String>> pairs = new HashMap<>();
+        HashMap<String, ArrayList<Double>> valuePairs = new HashMap<>();
+        for (int i = 0; i < equations.length; i++) {
+            pairs.computeIfAbsent(equations[i][0], k -> new ArrayList<String>()).add(equations[i][1]);
+            pairs.computeIfAbsent(equations[i][1], k -> new ArrayList<String>()).add(equations[i][0]);
+            valuePairs.computeIfAbsent(equations[i][0], k -> new ArrayList<Double>()).add(values[i]);
+            valuePairs.computeIfAbsent(equations[i][1], k -> new ArrayList<Double>()).add(1 / values[i]);
+        }
+        
+        double[] result = new double[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String[] query = queries[i];
+            result[i] = dfs(query[0], query[1], pairs, valuePairs, new HashSet<String>(), 1.0);
+            if (result[i] == 0.0) result[i] = -1.0;
+        }
+        return result;
+    }
+    
+    private double dfs(String start, String end, HashMap<String, ArrayList<String>> pairs, HashMap<String, ArrayList<Double>> values, HashSet<String> set, double value) {
+        if (set.contains(start)) return 0.0;
+        if (!pairs.containsKey(start)) return 0.0;
+        if (start.equals(end)) return value;
+        set.add(start);
+        
+        ArrayList<String> strList = pairs.get(start);
+        ArrayList<Double> valueList = values.get(start);
+        double tmp = 0.0;
+        for (int i = 0; i < strList.size(); i++) {
+            tmp = dfs(strList.get(i), end, pairs, values, set, value*valueList.get(i));
+            if (tmp != 0.0) {
+                break;
+            }
+        }
+        set.remove(start);
+        return tmp;
+    }
+}
