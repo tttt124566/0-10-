9e7e0a273f088441e063323285e77b9353c8a76f
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-21 17:11:10 -0700

    Create 159-LongestSubstringWithAtMostTwoDistinctCharacters.java

---
+++ 150-160/159-LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,34 @@
+/*
+Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
+
+Example 1:
+
+Input: "eceba"
+Output: 3
+Explanation: t is "ece" which its length is 3.
+Example 2:
+
+Input: "ccaabbb"
+Output: 5
+Explanation: t is "aabbb" which its length is 5.
+*/
+
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            while (map.size() > 2) {
+                map.put(s.charAt(start), map.get(s.charAt(start)) - 1);
+                if (map.get(s.charAt(start)) == 0) {
+                    map.remove(s.charAt(start));
+                }
+                start++;
+            }
+            maxLength = Math.max(i - start + 1, maxLength);
+        }
+        return maxLength;
+    }
+}
