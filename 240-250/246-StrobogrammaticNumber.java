2e9e5b4209bb0d7d92e004e4bedf5c8f839e2389
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-22 11:12:41 -0700

    Create 246-StrobogrammaticNumber.java

---
+++ 240-250/246-StrobogrammaticNumber.java
@@ -0,0 +1,39 @@
+/*
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+
+Example 1:
+
+Input:  "69"
+Output: true
+Example 2:
+
+Input:  "88"
+Output: true
+Example 3:
+
+Input:  "962"
+Output: false
+*/
+
+class Solution {
+    public boolean isStrobogrammatic(String num) {
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        map.put('6', '9');
+        map.put('9', '6');
+        map.put('8', '8');
+        map.put('1', '1');
+        map.put('0', '0');
+        int i = 0;
+        int j = num.length() - 1;
+        while (i <= j) {
+            if (!map.containsKey(num.charAt(i)) || map.get(num.charAt(i)) != num.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
