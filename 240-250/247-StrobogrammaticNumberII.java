e1b5d26e692823468d2d761f014118786c002bf5
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-22 12:00:19 -0700

    Create 247-StrobogrammaticNumberII.java

---
+++ 240-250/247-StrobogrammaticNumberII.java
@@ -0,0 +1,41 @@
+/*
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Find all strobogrammatic numbers that are of length = n.
+
+Example:
+
+Input:  n = 2
+Output: ["11","69","88","96"]
+*/
+
+class Solution {
+    public List<String> findStrobogrammatic(int n) {
+        return helper(n, n);
+    }
+    
+    private List<String> helper(int n, int m) {
+        if (n == 0) {
+            return new ArrayList<String>(Arrays.asList(""));
+        }
+        if (n == 1) {
+            return new ArrayList<String>(Arrays.asList("0", "1", "8"));
+        }
+        
+        List<String> list = helper(n - 2, m);
+        
+        List<String> res = new ArrayList<String>();
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if (n != m) {
+                res.add("0" + s + "0");
+            }
+            res.add("1" + s + "1");
+            res.add("6" + s + "9");
+            res.add("9" + s + "6");
+            res.add("8" + s + "8");
+        }
+        
+        return res;
+    }
+}
