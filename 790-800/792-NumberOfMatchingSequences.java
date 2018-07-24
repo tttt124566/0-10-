36ef095ffc8758d93c393a9bf893564b7c9d0a8c
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-23 19:59:09 -0700

    Create 792-NumberOfMatchingSequences.java

---
+++ 790-800/792-NumberOfMatchingSequences.java
@@ -0,0 +1,44 @@
+/*
+Given string S and a dictionary of words words, find the number of words[i] that is a subsequence of S.
+
+Example :
+Input: 
+S = "abcde"
+words = ["a", "bb", "acd", "ace"]
+Output: 3
+Explanation: There are three words in words that are a subsequence of S: "a", "acd", "ace".
+Note:
+
+All words in words and S will only consists of lowercase letters.
+The length of S will be in the range of [1, 50000].
+The length of words will be in the range of [1, 5000].
+The length of words[i] will be in the range of [1, 50].
+
+*/
+
+class Solution {
+    public int numMatchingSubseq(String S, String[] words) {
+        int count = 0;
+        for (String word : words) {
+            if (isSubsequence(S, word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    private boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        
+        return j == t.length();
+    }
+}
