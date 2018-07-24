4ed9bada932af342cb01ac272edfd0df8a88fbce
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-07-24 00:26:12 -0700

    Create 329-LongestIncreasingPathInAMatrix.java

---
+++ 320-330/329-LongestIncreasingPathInAMatrix.java
@@ -0,0 +1,62 @@
+/*
+Given an integer matrix, find the length of the longest increasing path.
+
+From each cell, you can either move to four directions: left, right, up or down. You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
+
+Example 1:
+
+Input: nums = 
+[
+  [9,9,4],
+  [6,6,8],
+  [2,1,1]
+] 
+Output: 4 
+Explanation: The longest increasing path is [1, 2, 6, 9].
+Example 2:
+
+Input: nums = 
+[
+  [3,4,5],
+  [3,2,6],
+  [2,2,1]
+] 
+Output: 4 
+Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+*/
+
+class Solution {
+    private int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int maxLen = 1;
+        int[][] cache = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                maxLen = Math.max(longestIncreasingPathHelper(matrix, i, j, cache), maxLen);
+            }
+        }
+        return maxLen;
+    }
+    
+    private int longestIncreasingPathHelper(int[][] matrix, int i, int j, int[][] cache) {
+        if (cache[i][j] != 0) {
+            return cache[i][j];
+        }
+        int maxLen = 1;
+        for (int[] dir : dirs) {
+            int x = dir[0] + i;
+            int y = dir[1] + j;
+            if (x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length || matrix[x][y] <= matrix[i][j]) {
+                continue;
+            }
+            maxLen = Math.max(maxLen, 1 + longestIncreasingPathHelper(matrix, x, y, cache));
+        }
+        cache[i][j] = maxLen;
+        return cache[i][j];
+    }
+}
