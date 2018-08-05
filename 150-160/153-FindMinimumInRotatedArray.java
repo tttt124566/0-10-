d4f95921c78addbb9a2043bde0e8e1d3f7ab23db
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-04 18:46:22 -0700

    Create 153-FindMinimumInRotatedArray.java

---
+++ 150-160/153-FindMinimumInRotatedArray.java
@@ -0,0 +1,36 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+Example 1:
+
+Input: [3,4,5,1,2] 
+Output: 1
+Example 2:
+
+Input: [4,5,6,7,0,1,2]
+Output: 0
+
+*/
+
+class Solution {
+    public int findMin(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] < nums[j]) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        
+        return nums[i];
+    }
+}
