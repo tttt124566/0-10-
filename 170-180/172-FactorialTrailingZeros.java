8a0eb62e59104107d8c9e6498995b0f624c52ed2
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-05 22:31:52 -0700

    Create 172-FactorialTrailingZeros.java

---
+++ 170-180/172-FactorialTrailingZeros.java
@@ -0,0 +1,22 @@
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Example 1:
+
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+
+*/
+
+//https://www.geeksforgeeks.org/count-trailing-zeroes-factorial-number/ 
+class Solution {
+    public int trailingZeroes(int n) {
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
+}
