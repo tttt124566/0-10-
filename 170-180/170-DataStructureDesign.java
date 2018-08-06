fe9ddaf0fb3029829ccb8cd158ceb2de5e5b0faf
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-05 21:49:23 -0700

    Update 170-DataStructureDesign.java

--- 170-180/170-DataStructureDesign.java
+++ 170-180/170-DataStructureDesign.java
@@ -56,3 +56,31 @@ class TwoSum {
  * obj.add(number);
  * boolean param_2 = obj.find(value);
  */
+
+/* another way */
+public class TwoSum {
+        Set<Integer> sum;
+        Set<Integer> num;
+        
+        TwoSum(){
+            sum = new HashSet<Integer>();
+            num = new HashSet<Integer>();
+        }
+        // Add the number to an internal data structure.
+    	public void add(int number) {
+    	    if(num.contains(number)){
+    	        sum.add(number * 2);
+    	    }else{
+    	        Iterator<Integer> iter = num.iterator();
+    	        while(iter.hasNext()){
+    	            sum.add(iter.next() + number);
+    	        }
+    	        num.add(number);
+    	    }
+    	}
+    
+        // Find if there exists any pair of numbers which sum is equal to the value.
+    	public boolean find(int value) {
+    	    return sum.contains(value);
+    	}
+    }
