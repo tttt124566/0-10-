36989f5764284176ec137610c6c485597725949b
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-16 21:15:45 -0700

    Update 261-GraphValidTree.java

--- 260-270/261-GraphValidTree.java
+++ 260-270/261-GraphValidTree.java
@@ -73,3 +73,71 @@ class Solution {
         return i;
     }
 }
+
+/* using 0, 1, 2, to differentiate not visited, visiting, visited.*/
+
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        int[] visited = new int[n];
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i=0; i<n; ++i) { 
+            adjList.add(new ArrayList<Integer>()); 
+        }
+        for (int[] edge: edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+        if (hasCycle(-1, 0, visited, adjList)) { 
+            return false; 
+        }  // has cycle
+        for (int v: visited) { 
+            if (v == 0) { 
+                return false; 
+            } 
+        }  // not 1 single connected component
+        return true;
+    }
+    
+    private boolean hasCycle(int pred, int vertex, int[] visited, List<List<Integer>> adjList) {
+        visited[vertex] = 1;  // current vertex is being visited
+        for (Integer succ: adjList.get(vertex)) {  // successors of current vertex
+            if (succ != pred) {  // exclude current vertex's predecessor
+                if (visited[succ] == 1) { return true; }  // back edge/loop detected!
+                else if (visited[succ] == 0) {
+                    if (hasCycle(vertex, succ, visited, adjList)) { 
+                        return true; 
+                    }
+                }
+            }
+        }
+        visited[vertex] = 2;
+        return false;
+    }
+}
+
+/* BFS */
+
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        int[] visited = new int[n];
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i=0; i<n; ++i) { adjList.add(new ArrayList<Integer>()); }
+        for (int[] edge: edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+        Deque<Integer> q = new ArrayDeque<>();
+        q.addLast(0); visited[0] = 1;  // vertex 0 is in the queue, being visited
+        while (!q.isEmpty()) {
+            Integer cur = q.removeFirst();
+            for (Integer succ: adjList.get(cur)) {
+                if (visited[succ] == 1) { return false; }  // loop detected
+                if (visited[succ] == 0) { q.addLast(succ); visited[succ] = 1; }
+            }
+            visited[cur] = 2;  // visit completed
+        }
+        for (int v: visited) { if (v == 0) { return false; } }  // # of connected components is not 1
+        return true;
+    }
+}
+
