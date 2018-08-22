2a5a1cbf2b9e13a4f173cbc519fc6dbec7f47ee6
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-21 22:44:30 -0700

    Create 311-SparseMatrixMultiplication.java

---
+++ 310-320/311-SparseMatrixMultiplication.java
@@ -0,0 +1,67 @@
+/*
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+Input:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+Output:
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+*/
+
+class Solution {
+    class Node {
+        int x;
+        int y;
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public int[][] multiply(int[][] A, int[][] B) {
+        int[][] result = new int[A.length][B[0].length];
+        List<Node> listA = new ArrayList<>();
+        List<Node> listB = new ArrayList<>();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] != 0) {
+                    listA.add(new Node(i, j));
+                }
+            }
+        }
+        
+        for (int i = 0; i < B.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                if (B[i][j] != 0) {
+                    listB.add(new Node(i, j));
+                }
+            }
+        }
+        
+        for (int i = 0; i < listA.size(); i++) {
+            for (int j = 0; j < listB.size(); j++) {
+                if (listA.get(i).y == listB.get(j).x) {
+                    result[listA.get(i).x][listB.get(j).y] = A[listA.get(i).x][listA.get(i).y] * B[listB.get(j).x][listB.get(j).y];
+                }
+            }
+        }
+        
+        return result;
+    }
+}
