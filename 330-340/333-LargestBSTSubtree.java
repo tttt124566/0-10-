0b5d24f0b6fabe74a30183bccd17dfaac13fc450
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-23 22:50:51 -0700

    Create 333-LargestBSTSubtree.java

---
+++ 330-340/333-LargestBSTSubtree.java
@@ -0,0 +1,58 @@
+/*
+Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), where largest means subtree with largest number of nodes in it.
+
+Note:
+A subtree must include all of its descendants.
+
+Example:
+
+Input: [10,5,15,1,8,null,7]
+
+   10 
+   / \ 
+  5  15 
+ / \   \ 
+1   8   7
+
+Output: 3
+Explanation: The Largest BST Subtree in this case is the highlighted one.
+             The return value is the subtree's size, which is 3.
+Follow up:
+Can you figure out ways to solve it with O(n) time complexity?
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    // return array for each node: 
+    //     [0] --> min
+    //     [1] --> max
+    //     [2] --> largest BST in its subtree(inclusive)
+    
+    public int largestBSTSubtree(TreeNode root) {
+        int[] ret = largestBST(root);
+        return ret[2];
+    }
+    
+    private int[] largestBST(TreeNode node){
+        if(node == null){
+            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
+        }
+        int[] left = largestBST(node.left);
+        int[] right = largestBST(node.right);
+        if(node.val > left[1] && node.val < right[0]){
+            return new int[]{Math.min(node.val, left[0]), Math.max(node.val, right[1]), left[2] + right[2] + 1};
+        }else{
+            return new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE, Math.max(left[2], right[2])};
+        }
+    }
+}
