be1f338c7c6bee96334e3b9086bb86df77cba54a
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-26 14:15:14 -0700

    Create 344-ReverseString.java

---
+++ 340-350/344-ReverseString.java
@@ -0,0 +1,28 @@
+/*
+Write a function that takes a string as input and returns the string reversed.
+
+Example 1:
+
+Input: "hello"
+Output: "olleh"
+Example 2:
+
+Input: "A man, a plan, a canal: Panama"
+Output: "amanaP :lanac a ,nalp a ,nam A"
+Seen this question in a real interview before?  
+
+*/
+
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]));
+            }
+        }
+        
+        return dp[n];
+    }
+}
