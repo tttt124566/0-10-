2eedd675bbb9b86807f1e6a4eac294664913310e
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-25 21:07:04 -0700

    Create 337-HouseRoberIII.java

---
+++ 330-340/337-HouseRoberIII.java
@@ -0,0 +1,61 @@
+/*
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+
+Output: 7 
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+
+Input: [3,4,5,1,3,null,1]
+
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+
+Output: 9
+Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = robHelper(root);
+        return Math.max(res[0], res[1]);
+    }
+    
+    private int[] robHelper(TreeNode root) {
+        if (root == null) {
+            return new int[] {0, 0};
+        } 
+        
+        int[] left = robHelper(root.left);
+        int[] right= robHelper(root.right);
+        
+        int include = root.val + left[1] + right[1];
+        int exclude = Math.max(left[1], left[0]) + Math.max(right[1], right[0]);
+        
+        return new int[] {include, exclude};
+    }
+}
