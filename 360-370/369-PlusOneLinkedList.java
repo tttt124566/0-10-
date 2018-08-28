0600c214298f7dcd4b3b59886bbd762de3005d10
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-27 20:40:32 -0700

    Create 369-PlusOneLinkedList.java

---
+++ 360-370/369-PlusOneLinkedList.java
@@ -0,0 +1,51 @@
+/*
+Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+Input:
+1->2->3
+
+Output:
+1->2->4
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode plusOne(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode i = dummy;
+        ListNode j = dummy;
+
+        while (j.next != null) {
+            j = j.next;
+            if (j.val != 9) {
+                i = j;
+            }
+        }
+        
+        i.val++;
+        i = i.next;
+        while(i != null) {
+            i.val = 0;
+            i = i.next;
+        }
+        
+        if (dummy.val == 0) {
+            return dummy.next;
+        } else {
+            return dummy;
+        }
+    }
+}
