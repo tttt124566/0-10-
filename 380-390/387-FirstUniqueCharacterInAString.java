84d05d359e03306aa9ca31e3e6d18b2f5dc97f7b
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-08-28 17:03:59 -0700

    Update 387-FirstUniqueCharacterInAString.java

--- 380-390/387-FirstUniqueCharacterInAString.java
+++ 380-390/387-FirstUniqueCharacterInAString.java
@@ -26,27 +26,18 @@ Special thanks to @agave and @StefanPochmann for adding this problem and creatin
 
 */
 
-We are playing the Guess Game. The game is as follows:
-
-I pick a number from 1 to n. You have to guess which number I picked.
-
-Every time you guess wrong, I'll tell you whether the number I picked is higher or lower.
-
-However, when you guess a particular number x, and you guess wrong, you pay $x. You win the game when you guess the number I picked.
-
-Example:
-
-n = 10, I pick 8.
-
-First round:  You guess 5, I tell you that it's higher. You pay $5.
-Second round: You guess 7, I tell you that it's higher. You pay $7.
-Third round:  You guess 9, I tell you that it's lower. You pay $9.
-
-Game over. 8 is the number I picked.
-
-You end up paying $5 + $7 + $9 = $21.
-Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.
-
-Credits:
-Special thanks to @agave and @StefanPochmann for adding this problem and creating all test cases.
-
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
