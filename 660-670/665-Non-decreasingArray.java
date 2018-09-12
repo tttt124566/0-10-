082b970a2bbcf88629c6772ef7644e59c8dc423b
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-09-11 22:28:38 -0700

    Update 665-Non-decreasingArray.java

--- 660-670/665-Non-decreasingArray.java
+++ 660-670/665-Non-decreasingArray.java
@@ -22,10 +22,10 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] > nums[i]) {
                 cnt++;
-                if (i - 2 < 0 || nums[i - 2] >= nums[i]) {
-                    nums[i - 1] = nums[i];
-                } else {
+                if (i - 2 >= 0 && nums[i - 2] >= nums[i]) {
                     nums[i] = nums[i - 1];
+                } else {
+                    nums[i - 1] = nums[i];
                 }
             }
         }
