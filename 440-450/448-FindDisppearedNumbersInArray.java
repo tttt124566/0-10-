7135d34ead8720c2966a77f9663dfd6ac8f2f627
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-09-24 23:42:38 -0700

    Update and rename 448- to 448-FindDisppearedNumbersInArray.java

--- 440-450/448-FindDisppearedNumbersInArray.java
+++ 440-450/448-FindDisppearedNumbersInArray.java
@@ -13,6 +13,23 @@ Input:
 Output:
 [5,6]
 */
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> disappearedNums;
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != i + 1) {
+                disappearedNums.push_back(i + 1);
+            }
+        }
+        return disappearedNums;
+    }
+};
 
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
