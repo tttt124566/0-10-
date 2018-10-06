85bf336704f0329f7bae8cf072212db97d2d9891
tttt124566 <40189981+tttt124566@users.noreply.github.com>
2018-10-06 15:04:37 -0700

    Update and rename GroupAnagrams.java to 49-GroupAnagrams.java

---
+++ 40-50/49-GroupAnagrams.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        for(String str : strs) {
+            char[] strArray = str.toCharArray();
+            Arrays.sort(strArray);
+            String sortedStr = new String(strArray);
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<String>());
+            }
+            map.get(sortedStr).add(str);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
